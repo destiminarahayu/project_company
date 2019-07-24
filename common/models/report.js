c97295c6d0a55e560036c9ae831b0106476d9ea5
destiminarahayu <destiminarahayu@gmail.com>
2019-07-24 17:10:36 +0700

    tes

--- common/models/report.js
+++ common/models/report.js
@@ -3,7 +3,69 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-module.exports = function(Report) {
+var app = require('../../server/server')
+module.exports = function(Report) {  
+
+    Report.remoteMethod(
+        'getByid',{
+            description:'get id  like -> Number',
+            accepts:[
+                {arg: 'id', type:'string'}
+            ],
+            returns:{
+                arg:'id', type:'object', root: true
+            },
+            http:{path: '/getByid', verb:'get'}
+        }
+     );
+
+     Report.getByid = function(id, callback){
+        new Promise(function(resolve, reject){
+            var filter ={
+                where :{
+                    id : {
+                        like : id
+                    }
+                }
+            }
+            Report.find(filter,function(err,result){
+                if (err) reject (err)
+                if (result===null){
+                    err = new Error('Cannot find that id')
+                    err.statusCode = 404
+                    reject (err)
+                }
+                resolve (result)
+            })
+        }).then(function(res){
+            var client = app.models.Client
+
+            var clientId = res[0].id_client
+            var filter = {
+                where : {
+                    id : clientId
+                }
+            }
+
+            //console log(filter)
+
+            client.find(filter, function(err, resclient){
+                if (err) return (err)
+                if (resclient===null) {
+                    err =new Error('cannot find that name')
+                    err.statusCode = 404
+                    return(err)
+                }
+                // console.log ('2', resclient)
+                res[0].client = resclient[0]
+                return callback(null,res)
+            })
+        }).catch(function(err){
+            callback(err);
+        })
+
+    },
+
     Report.remoteMethod(
         'getnoTransaksi',{
             description:'get no transaksi like -> desti',
@@ -42,5 +104,7 @@ module.exports = function(Report) {
             callback(err)
         });
     }
+
+  
 
 };
